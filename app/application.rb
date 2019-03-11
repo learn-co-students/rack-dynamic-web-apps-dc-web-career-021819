85d02dfcfcee83fb1b7d2915c2d8284342c1806f
bmanek <41875460+bmanek@users.noreply.github.com>
2019-03-11 10:35:46 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,30 @@
 class Application
 
   def call(env)
+    # def resp
+
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    # set random nums
+
+    num1 = Kernel.rand(1..10)
+    num2 = Kernel.rand(1..10)
+    num3 = Kernel.rand(1..10)
+
+    # print numbers to window
+
+    resp.write "#{num1}"
+    resp.write "#{num2}"
+    resp.write "#{num3}"
+
+    # create control flow for win/lose
+
+    if num1 == num2 && num2 == num3
+      resp.write "You Win!"
+    else
+      resp.write "You Lose."
+    end
+
     resp.finish
   end
 
